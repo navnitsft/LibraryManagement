98717bfdbbbad4a21be5937d6a30389c0717da57
navnitsft <navnitsft@gmail.com>
2025-09-09 12:00:28 +0530

    Added BooksController. Added Index.cshtml in Books view. Modified the _Layout.cshtml.

---
+++ LibraryManagement/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BooksController(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _libraryContext.Books.ToListAsync();
+            return View(books);
+        }
+    }
+}
